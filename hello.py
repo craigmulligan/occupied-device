209c624d0e0ab015051c3b9e9574bc23cd4909da
Craig Mulligan <craig@firingsuad.co.za>
2015-02-13 00:11:33 +0000

    new commit

--- hello.py
+++ hello.py
@@ -10,17 +10,15 @@ GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 	 
 while True:
     input_state = GPIO.input(17)
-    if input_state == True:
-		print('Button Pressed')
-		fb_state = occupiedo.check_door()
-		if fb_state == "true":
-			state = True
-		else:
-			state = False
-		print "input_state: " + str(input_state)
-		print "fb_state: " + str(state)
-		#check if physical doors input is the same as firebase if not - update firebase
-		if state != input_state:
-			#door state has changed so fire off actions
-			occupiedo.change_occupied_state(fb_state)
-		time.sleep(0.2)
+	fb_state = occupiedo.check_door()
+	if fb_state == "true":
+		state = 1
+	else:
+		state = 0
+	print "input_state: " + str(input_state)
+	print "fb_state: " + str(state)
+	#check if physical doors input is the same as firebase if not - update firebase
+	if state != input_state:
+		#door state has changed so fire off actions
+		occupiedo.change_occupied_state(fb_state)
+	time.sleep(0.2)

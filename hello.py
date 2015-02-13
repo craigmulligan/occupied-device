b7756fd47a00d35763d38987f3263e43925aedb6
Craig Mulligan <craig@firingsuad.co.za>
2015-02-13 00:07:45 +0000

    new commit

--- hello.py
+++ hello.py
@@ -18,9 +18,9 @@ while True:
 		else:
 			state = False
 		print "input_state: " + str(input_state)
-		print "current_state: " + str(state)
+		print "fb_state: " + str(state)
 		#check if physical doors input is the same as firebase if not - update firebase
-		if state == input_state:
+		if state != input_state:
 			#door state has changed so fire off actions
 			occupiedo.change_occupied_state(fb_state)
 		time.sleep(0.2)
